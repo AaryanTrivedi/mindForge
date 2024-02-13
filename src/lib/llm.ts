03472b3aaec0d5fafcda4f1f0dbe6439edfce8db
Aaryan Trivedi <itsaaryantrivedi@gmail.com>
2024-02-14 02:01:43 +0530

    Refactor: Improve data handling in LLM
    Changes made:
    - Added streaming factors
    - Removed JSON parameter
    - added Verbose and Temprature parameters

--- src/lib/llm.ts
+++ src/lib/llm.ts
@@ -1,14 +1,17 @@
 import { ChatOllama } from "@langchain/community/chat_models/ollama"
 
 export const streamingModel = new ChatOllama({
-    baseUrl: "http://localhost:11434/",
-    model: "mistral",
-    format: "json",
-    
-  });
-  
-  export const nonStreamingModel = new ChatOllama({
-    baseUrl: "http://localhost:11434/",
-    model: "mistral",
-    format: "json",
-  });
+  baseUrl: "http://localhost:11434/",
+  model: "mistral",
+  streaming: true,
+  verbose: true,
+  temperature: 0,
+});
+
+
+export const nonStreamingModel = new ChatOllama({
+  baseUrl: "http://localhost:11434/",
+  model: "mistral",
+  verbose: true,
+  temperature: 0,
+});
